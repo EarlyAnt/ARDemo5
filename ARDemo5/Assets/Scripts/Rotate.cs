5d4ba7a14ab98ead74ef3df41c14f4b6c8ed44a6
EarlyAnt <54763755@qq.com>
2019-10-15 17:08:40 +0800

    1.提交第1版代码。 2.已经实现了扫描2张图片，识别后显示3D物体的功能。

---
+++ ARDemo5/Assets/Scripts/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField, Range(0.1f, 10f)]
+    float speedX = 0.1f;
+    [SerializeField, Range(0.1f, 10f)]
+    float speedY = 0.1f;
+    [SerializeField, Range(0.1f, 10f)]
+    float speedZ = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(this.speedX, this.speedY, this.speedZ));
+    }
+}
